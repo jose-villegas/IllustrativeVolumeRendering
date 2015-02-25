43d6f7230f779b1388206bb59526f3c49115c601
jose-villegas <polonoise@gmail.com>
2015-02-24 23:40:48 -0430

    screens

--- FGC_T3/RawDataModel.cpp
+++ FGC_T3/RawDataModel.cpp
@@ -170,7 +170,7 @@ bool RawDataModel::createBackFaceTexture()
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16F, MainData::rootWindow->getSize().x, MainData::rootWindow->getSize().y, 0, GL_RGBA, GL_FLOAT,
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, MainData::rootWindow->getSize().x, MainData::rootWindow->getSize().y, 0, GL_RGBA, GL_FLOAT,
                      NULL);
     } catch (...) {
         return false;
---
+++ FGC_T3/Resources/metal.tf
@@ -0,0 +1,20 @@
+{
+    "Control Points":
+    [
+        {
+            "IsoValue":0,
+            "Opacity":0,
+            "Style":0
+        },
+        {
+            "IsoValue":45,
+            "Opacity":0,
+            "Style":29
+        },
+        {
+            "IsoValue":255,
+            "Opacity":255,
+            "Style":4
+        }
+    ]
+}
